cffd26816fc6792c0f4a3103a22c93944b9e2ac0
tung <tngu0492@student.monash.edu>
2025-08-11 12:18:14 +1000

    Implement the rest of vehicle census data to the app

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
   { path: '/', redirect: '/insights' },
-  { path: '/insights', name: 'Insights', component: () => import('../views/DataInsightsView.vue') },
+  { path: '/insights', name: 'Insights', component: () => import('../views/InsightsView.vue') },
   { path: '/find', name: 'FindParking', component: () => import('../views/FindParkingView.vue') },
   { path: '/forecast', name: 'ParkingForecast', component: () => import('../views/ParkingForecastView.vue') },
   { path: '/history', name: 'ParkingHistory', component: () => import('../views/ParkingHistoryView.vue') },
