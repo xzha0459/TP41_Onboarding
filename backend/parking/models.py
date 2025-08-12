5d5d881722c43a6ffc3d65175a52d8f7f7b94cc8
Matthewheekh <66135287+Matthewheekh@users.noreply.github.com>
2025-08-12 22:04:00 +1000

    Added parking sign information in apis (#2)

--- backend/parking/models.py
+++ backend/parking/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
 class Parking(models.Model):
     kerbside_id = models.CharField(primary_key=True, max_length=50)
     zone_number = models.CharField(max_length=20)
@@ -10,9 +8,13 @@ class Parking(models.Model):
     latitude = models.FloatField()
     longitude = models.FloatField()
     last_updated = models.DateTimeField()
+    sign_text = models.CharField(max_length=500, null=True, blank=True)
+    days_of_week = models.CharField(max_length=50, null=True, blank=True)
+    start_time = models.TimeField(null=True, blank=True)
+    end_time = models.TimeField(null=True, blank=True)
     
     class Meta:
-        db_table = 'vw_api_bay_list'
+        db_table = 'vw_api_bay_list_with_sign'
     
     @property   
     def is_occupied(self):
